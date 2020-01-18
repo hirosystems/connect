0a9d75222b308ff2838ee31862628f7e9eee1a95
Hank Stoever <hstove@gmail.com>
2020-01-18 10:34:11 -0800

    first pass at vanilla JS auth

---
+++ src/ui.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { useConnect, Connect } from './react';
+import { AuthOptions } from './auth';
+
+const Base: React.FC = () => {
+  const { doOpenDataVault } = useConnect();
+
+  useEffect(() => {
+    doOpenDataVault();
+  }, []);
+
+  return <div />;
+};
+
+export const showBlockstackConnect = (authOptions: AuthOptions) => {
+  const baseDiv = document.createElement('div');
+  document.body.appendChild(baseDiv);
+  ReactDOM.render(
+    <Connect authOptions={authOptions}>
+      <Base />
+    </Connect>,
+    baseDiv
+  );
+};
