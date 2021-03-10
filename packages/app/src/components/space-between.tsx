4ca4617b7be0ee15151182380906ad1d3141717d
Hank Stoever <hstove@gmail.com>
2021-03-10 14:22:12 -0800

    feat: remove app, test-app, rpc-client packages

--- packages/app/src/components/space-between.tsx
+++
@@ -1,6 +0,0 @@
-import { Flex, FlexProps } from '@stacks/ui';
-import React from 'react';
-
-export const SpaceBetween: React.FC<FlexProps> = props => (
-  <Flex alignItems="center" justifyContent="space-between" {...props} />
-);
--- packages/test-app/common/use-stx-address.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AppContext } from '@common/context';
-
-export const useSTXAddress = (): string | undefined => {
-  const { userData } = useContext(AppContext);
-  return userData?.profile?.stxAddress?.testnet as string;
-};
