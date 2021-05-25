da5b27720d2e7f6a77829f94303c985d85bfb63b
fbwoolf <fwoolf@blockstack.com>
2021-05-25 15:15:38 -0500

    feat: expose profile url in auth payload

---
+++ .changeset/shaggy-kids-unite.md
@@ -0,0 +1,5 @@
+---
+'@stacks/connect': major
+---
+
+This exposes the profile_url and other properties in the auth response. It also removes the deprecated 'finished' prop from AuthOptions as a breaking change. We will now only support the use of 'onFinish'.
--- packages/connect/src/types/auth.ts
+++ packages/connect/src/types/auth.ts
@@ -1,7 +1,26 @@
 import type { UserSession } from '@stacks/auth';
 
-export interface FinishedData {
+export interface AuthResponsePayload {
+  private_key: string;
+  username: string | null;
+  hubUrl: string;
+  associationToken: string;
+  blockstackAPIUrl: string | null;
+  core_token: string | null;
+  email: string | null;
+  exp: number;
+  iat: number;
+  iss: string;
+  jti: string;
+  version: string;
+  profile: any;
+  profile_url: string;
+  public_keys: string[];
+}
+
+export interface FinishedAuthData {
   authResponse: string;
+  authResponsePayload: AuthResponsePayload;
   userSession: UserSession;
 }
 
@@ -17,15 +36,14 @@ export interface AuthOptions {
   /** The URL you want the user to be redirected to after authentication. */
   redirectTo?: string;
   manifestPath?: string;
-  /** @deprecated use `onFinish` */
-  finished?: (payload: FinishedData) => void;
   /**
    * This callback is fired after authentication is finished.
-   * The callback is called with a single object argument, with two keys:
-   * `userSession`: a UserSession object with `userData` included
+   * The callback is called with a single object argument, with three keys:
    * `authResponse`: the raw `authResponse` string that is returned from authentication
+   * `authResponsePayload`: an AuthResponsePayload object
+   * `userSession`: a UserSession object with `userData` included
    * */
-  onFinish?: (payload: FinishedData) => void;
+  onFinish?: (payload: FinishedAuthData) => void;
   /** This callback is fired if the user exits before finishing */
   onCancel?: () => void;
   /**
