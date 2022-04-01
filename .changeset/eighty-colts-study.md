b4900985624641510f3b93d8a0286cca93b72085
beguene <beguene@gmail.com>
2022-04-01 11:26:11 +0200

    feat:  support arbitrary message signing

---
+++ .changeset/eighty-colts-study.md
@@ -0,0 +1,6 @@
+---
+'@stacks/connect': minor
+'@stacks/connect-react': minor
+---
+
+Add helpers to request a message (string) signature
---
+++ packages/connect/src/signature/index.ts
@@ -0,0 +1,94 @@
+import { TokenSigner } from 'jsontokens';
+import { ChainID } from '@stacks/transactions';
+import { getStacksProvider } from '../utils';
+import { StacksTestnet } from '@stacks/network';
+import {
+  SignatureData,
+  SignatureOptions,
+  SignaturePayload,
+  SignaturePopup,
+  SignatureRequestOptions,
+} from 'src/types/signature';
+import { getKeys, getUserSession } from '../transactions';
+
+function getStxAddress(options: SignatureRequestOptions) {
+  const { userSession, network } = options;
+
+  if (!userSession || !network) return undefined;
+  const stxAddresses = userSession?.loadUserData().profile?.stxAddress;
+  const chainIdToKey = {
+    [ChainID.Mainnet]: 'mainnet',
+    [ChainID.Testnet]: 'testnet',
+  };
+  const address: string | undefined = stxAddresses?.[chainIdToKey[network.chainId]];
+  return address;
+}
+
+function getDefaultSignatureRequestOptions(options: SignatureRequestOptions) {
+  const network = options.network || new StacksTestnet();
+  const userSession = getUserSession(options.userSession);
+  const defaults: SignatureRequestOptions = {
+    ...options,
+    network,
+    userSession,
+  };
+  return {
+    stxAddress: getStxAddress(defaults),
+    ...defaults,
+  };
+}
+
+async function signPayload(payload: SignaturePayload, privateKey: string) {
+  const tokenSigner = new TokenSigner('ES256k', privateKey);
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  return tokenSigner.signAsync({
+    ...payload,
+  } as any);
+}
+
+async function openSignaturePopup({ token, options }: SignaturePopup) {
+  const provider = getStacksProvider();
+  if (!provider) {
+    throw new Error('Hiro Wallet not installed.');
+  }
+
+  try {
+    const signatureResponse = await provider.signatureRequest(token);
+
+    options.onFinish?.(signatureResponse as SignatureData);
+  } catch (error) {
+    console.error('[Connect] Error during signature request', error);
+    options.onCancel?.();
+  }
+}
+
+export interface SignatureRequestPayload {
+  message: string;
+}
+
+export const signMessage = async (options: SignatureRequestOptions) => {
+  const { userSession, ..._options } = options;
+  const { privateKey, publicKey } = getKeys(userSession);
+
+  const payload: SignaturePayload = {
+    ..._options,
+    publicKey,
+  };
+
+  return signPayload(payload, privateKey);
+};
+
+async function generateTokenAndOpenPopup<T extends SignatureOptions>(
+  options: T,
+  makeTokenFn: (options: T) => Promise<string>
+) {
+  const token = await makeTokenFn({
+    ...getDefaultSignatureRequestOptions(options),
+    ...options,
+  } as T);
+  return openSignaturePopup({ token, options });
+}
+
+export function openSignatureRequestPopup(options: SignatureRequestOptions) {
+  return generateTokenAndOpenPopup(options, signMessage);
+}
