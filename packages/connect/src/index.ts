1f3e94aa09a356cf8d8a0b555caeabbed120d741
beguene <beguene@gmail.com>
2022-05-02 12:18:19 +0200

    feat: add helpers to sign structured data

--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,6 +1,7 @@
 export * from './auth';
 export * from './transactions';
 export * from './signature';
+export * from './signature/structuredData';
 export * from './types';
 export * from './utils';
 export * from './ui';
---
+++ packages/connect/src/signature/structuredData.ts
@@ -0,0 +1,65 @@
+import { getStacksProvider } from '../utils';
+import { getKeys } from '../transactions';
+import { getDefaultSignatureRequestOptions } from '.';
+import {
+  StructuredDataSignatureOptions,
+  StructuredDataSignaturePayload,
+  StructuredDataSignaturePopup,
+  StructuredDataSignatureRequestOptions,
+} from 'src/types/structuredDataSignature';
+import { TokenSigner } from 'jsontokens';
+import { serializeCV } from '@stacks/transactions';
+
+async function generateTokenAndOpenPopup<T extends StructuredDataSignatureOptions>(
+  options: T,
+  makeTokenFn: (options: T) => Promise<string>
+) {
+  const token = await makeTokenFn({
+    ...getDefaultSignatureRequestOptions(options),
+    ...options,
+  } as T);
+  return openStructuredDataSignaturePopup({ token, options });
+}
+
+async function signPayload(payload: StructuredDataSignaturePayload, privateKey: string) {
+  const tokenSigner = new TokenSigner('ES256k', privateKey);
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  return tokenSigner.signAsync({
+    ...payload,
+    message: serializeCV(payload.message).toString('hex'),
+  } as any);
+}
+
+export async function signStructuredMessage(options: StructuredDataSignatureRequestOptions) {
+  const { userSession, ..._options } = options;
+  const { privateKey, publicKey } = getKeys(userSession);
+
+  const payload: StructuredDataSignaturePayload = {
+    ..._options,
+    publicKey,
+  };
+
+  return signPayload(payload, privateKey);
+}
+
+async function openStructuredDataSignaturePopup({ token, options }: StructuredDataSignaturePopup) {
+  const provider = getStacksProvider();
+  if (!provider) {
+    throw new Error('Hiro Wallet not installed.');
+  }
+
+  try {
+    const signatureResponse = await provider.structuredDataSignatureRequest(token);
+
+    options.onFinish?.(signatureResponse);
+  } catch (error) {
+    console.error('[Connect] Error during signature request', error);
+    options.onCancel?.();
+  }
+}
+
+export function openStructuredDataSignatureRequestPopup(
+  options: StructuredDataSignatureRequestOptions
+) {
+  return generateTokenAndOpenPopup(options, signStructuredMessage);
+}
