3702aa0c0d1d436a7eef09e5bc49331e81f33736
janniks <janniks@users.noreply.github.com>
2022-06-22 16:15:42 +0200

    fix: revert commit 425e771, static show connect
    This reverts commit 425e771f2d07b45254a9c63387a07480a0dee2c1.

---
+++ .changeset/short-clocks-poke.md
@@ -0,0 +1,6 @@
+---
+'@stacks/connect': patch
+'@stacks/connect-ui': patch
+---
+
+Revert adding a static showConnect alternative
--- packages/connect/src/ui.ts
+++ packages/connect/src/ui.ts
@@ -1,7 +1,6 @@
 import { authenticate } from './auth';
 import type { AuthOptions } from './types/auth';
 import { defineCustomElements } from '@stacks/connect-ui/loader';
-import { ConnectModal } from '@stacks/connect-ui/dist/components/connect-modal';
 import { getStacksProvider } from './utils';
 
 export const showConnect = (authOptions: AuthOptions) => {
@@ -11,31 +10,6 @@ export const showConnect = (authOptions: AuthOptions) => {
   }
   if (typeof window !== undefined) {
     void defineCustomElements(window);
-    const element = document.createElement('connect-modal');
-    element.authOptions = authOptions;
-    document.body.appendChild(element);
-    const handleEsc = (ev: KeyboardEvent) => {
-      if (ev.key === 'Escape') {
-        document.removeEventListener('keydown', handleEsc);
-        element.remove();
-      }
-    };
-    document.addEventListener('keydown', handleEsc);
-  }
-};
-
-export const showConnectStatic = (authOptions: AuthOptions) => {
-  if (getStacksProvider()) {
-    void authenticate(authOptions);
-    return;
-  }
-
-  if (typeof window?.customElements?.define === 'function') {
-    // supports custom elements
-    if (customElements.get('connect-modal') === undefined) {
-      // avoid repeat defines
-      customElements.define('connect-modal', ConnectModal);
-    }
     const element = document.createElement('connect-modal');
     element.authOptions = authOptions;
     document.body.appendChild(element);
