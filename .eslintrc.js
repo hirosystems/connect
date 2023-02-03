f5e329af7ff3a22642fd4a531985c1f61a7d1688
janniks <janniks@users.noreply.github.com>
2023-02-03 16:54:50 +0100

    chore: merge main into branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,4 @@ module.exports = {
   extends: ['plugin:@stencil-community/recommended', '@stacks/eslint-config'],
   plugins: ['react', 'react-hooks'],
   root: true,
-  rules: {
-    // '@typescript-eslint/require-await': [0],
-    // '@typescript-eslint/no-unnecessary-type-assertion': [0],
-  },
 };
