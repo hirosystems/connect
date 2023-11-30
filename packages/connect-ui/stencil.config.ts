baec1f7ed2d8d1724b9cee80c907f4da9ab96296
janniks <janniks@users.noreply.github.com>
2023-11-30 17:18:34 +0100

    build: minify tailwind

--- packages/connect-ui/stencil.config.ts
+++ packages/connect-ui/stencil.config.ts
@@ -1,8 +1,30 @@
 import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
-import tailwind, { tailwindHMR, PluginOpts } from 'stencil-tailwind-plugin';
+import tailwind, {
+  PluginOpts,
+  setPluginConfigurationDefaults,
+  tailwindHMR,
+} from 'stencil-tailwind-plugin';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import rename from 'postcss-rename';
 
 import tailwindConf from './tailwind.config.ts';
+
+setPluginConfigurationDefaults({
+  ...PluginOpts.DEFAULT,
+  enableDebug: true,
+  stripComments: true,
+  tailwindConf,
+  postcss: {
+    plugins: [
+      tailwindcss(),
+      autoprefixer(),
+      rename({
+        strategy: 'minimal'
+      })
+    ]
+  }
+});
 
 export const config: Config = {
   namespace: 'connect-ui',
@@ -21,14 +43,7 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
-  plugins: [
-    sass(),
-    tailwind({
-      ...PluginOpts.DEFAULT,
-      tailwindConf,
-    }),
-    tailwindHMR(),
-  ],
+  plugins: [tailwind(), tailwindHMR()],
   extras: {
     experimentalImportInjection: true,
   },
