d6ea424331a022be0bf38e95a04832e050c0c143
janniks <6362150+janniks@users.noreply.github.com>
2025-01-08 17:11:47 +0100

    feat: add asigna iframe connect (#400)
    * added integration with Asigna via iframe
    
    * changed asigna provider data
    
    * fixed check for asigna iframe
    
    * chore: update lint errors
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: jeffrystone <vpromotorov@gmail.com>
    Co-authored-by: janniks <janniks@users.noreply.github.com>

---
+++ .changeset/smart-dingos-train.md
@@ -0,0 +1,6 @@
+---
+'@stacks/connect-ui': minor
+'@stacks/connect': minor
+---
+
+Add iframe capable Asigna provider.
---
+++ packages/connect/src/asigna.ts
@@ -0,0 +1,33 @@
+const source = 'asigna-stx';
+
+const generateCall = (payload: string, key: string) => {
+  return new Promise(res => {
+    function listener(message: MessageEvent<any>) {
+      if (message.data.source === source && message.data[key]) {
+        res(message.data[key]);
+        window.removeEventListener('message', listener);
+      }
+    }
+    window.addEventListener('message', listener);
+    window.top.postMessage(generateAsignaMessage(payload, key), '*');
+  });
+};
+
+const AsignaIframeProvider = {
+  authenticationRequest: async (payload: string) => {
+    return generateCall(payload, 'authenticationRequest');
+  },
+  transactionRequest: async (payload: string) => {
+    return generateCall(payload, 'transactionRequest');
+  },
+};
+const generateAsignaMessage = (payload: string, key: string) => {
+  return { source, [key]: payload };
+};
+
+export const initializeAsignaProvider = () => {
+  const isAsignaIframe = !!window.top && document.referrer.endsWith('.asigna.io/');
+  if (isAsignaIframe) {
+    window['AsignaProvider'] = AsignaIframeProvider;
+  }
+};
